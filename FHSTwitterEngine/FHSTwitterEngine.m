a70944f4b4868c502b2dc8dfff6dc7f7a263d652
Victor Weng <victor.weng@gmail.com>
2014-03-10 22:58:52 -0400

    fixed mem leak

--- FHSTwitterEngine/FHSTwitterEngine.m
+++ FHSTwitterEngine/FHSTwitterEngine.m
@@ -274,7 +274,7 @@ id removeNull(id rootObject) {
 
 - (NSString *)fhs_URLEncode {
     CFStringRef url = CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (CFStringRef)self, NULL, CFSTR("!*'();:@&=+$,/?%#[]"), kCFStringEncodingUTF8);
-	return (__bridge NSString *)url;
+	return (__bridge_transfer NSString *)url;
 }
 
 - (NSString *)fhs_truncatedToLength:(int)length {
